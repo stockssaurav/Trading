8bee78f94d2d4c955a356cda8eaa82990eaa9cc6
stockssaurav <146532590+stockssaurav@users.noreply.github.com>
2023-09-30 15:22:24 +0530

    index.js

---
+++ .github/index.js
@@ -0,0 +1,7 @@
+function yourlink() {
+
+			var locs = ['bank', 'finance', 'pharma', 'auto', 'commodities', 'fmcg', 'infra', 'it', 'realty', 'media', 'mnc', 'energy', 'metal']; 
+
+			for (let i = 0; i < locs.length; i++) {
+				window.open('https://archives.nseindia.com/content/indices/ind_nifty'+locs[i]+'list.csv')}
+			};
